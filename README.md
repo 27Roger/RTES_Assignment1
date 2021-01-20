c07ac2710188ca009d513ed093a6d406bfd11d6a
michaelthoreau2 <michael@thoreau.com.au>
2021-01-20 13:55:41 -0500

    added structure

---
+++ README.md
@@ -0,0 +1,5 @@
+# Real Time Embedded Systems
+### Programming Assignment 1 - Introduction
+
+
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,30 @@
 #include <mbed.h>
 
+void part1();
+void extension();
+
+
 int main() {
 
-  // put your setup code here, to run once:
+  // part 1 of the assignment
+  part1();
+
+  // extension component
+  extension();
+
+
 
   while(1) {
-    // put your main code here, to run repeatedly:
+    
   }
+}
+
+
+void part1() {
+  
+}
+
+
+void extension () {
+
 }
