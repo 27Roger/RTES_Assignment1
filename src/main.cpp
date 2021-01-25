816e3e12cd23659e77b01897d2fd8d1886e76f47
piekarski92 <76534954+piekarski92@users.noreply.github.com>
2021-01-25 11:34:44 -0500

    Added extension potion of assignment

--- src/main.cpp
+++ src/main.cpp
@@ -16,19 +16,20 @@ void extension();
 DigitalOut led1(LED1);
 DigitalOut led2(LED1);
 
-string test = "";
+
 
 // Variables Used for Part 2/Extension
 char secretString[16] = {0x57,0x65,0x6C,0x63,0x6F,0x6D,0x65,0x20,0x74,0x6F,0x20,0x52,0x54,0x45,0x53,0x21};
+double result = 1234;
 
 
 int main() {
 
 	// part 1 of the assignment
-	part1();
+	//part1();
 
 	// part 1 of the assignment
-	part1b();
+	//part1b();
 
 	// part 2 of the assignment
 	part2();
@@ -94,14 +95,25 @@ void part1b() {
 */
 void part2() {
   uint8_t i;
+  string decode = "";
 
   for(i = 0; i < 16; ++i){
-    test = secretString[i];
+    decode = secretString[i];
   }
 
 }
 
-
+/*
+*	Extension - Using the 1st letter decoded in part 2. Find the number
+*	that corresponds to the letter (a=1, b=2, ... , y=25, z=26) and input
+*	the number in the below function for the 'key' variable. Set an 
+*	appropriate breakpoint and report the value of the 'result' variable.
+*
+* 	*Hint* - When setting a breakpoint, respect that result is a global variable.
+*/
 void extension () {
+	int f = 380489;
+	int key = 0;
+	result = fmod(f,key);
 
 }
