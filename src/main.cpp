fc9502c391cd180d8f910ba40aed4a1f597fdfd9
michaelthoreau <mjt9978@nyu.edu>
2021-01-29 15:33:21 -0500

    modified part2, added print statement (part1c)

--- src/main.cpp
+++ src/main.cpp
@@ -7,31 +7,52 @@
 #include <mbed.h>
 #include <string.h>
 
+
+/* 	Include for printing via the micro USB port
+*	This requires you have the latest st-link driver:
+*   https://www.st.com/en/development-tools/stsw-link009.html
+*	See a TA for help if you cannot print via this method - and remove the following lines
+*/
+#include <USBSerial.h>
+
+// Important!  -  comment out this line if the debugger gets stuck here
+USBSerial serial;
+
+
+
+// Function Prototypes
 void part1();
 void part1b();
+void part1c();
 void part2();
 void extension();
 
-// define LEDs
+
+// Define LEDs
 DigitalOut led1(LED1);
-DigitalOut led2(LED1);
+DigitalOut led2(LED2);
 
 
 
 // Variables Used for Part 2/Extension
-char secretString[16] = {0x57,0x65,0x6C,0x63,0x6F,0x6D,0x65,0x20,0x74,0x6F,0x20,0x52,0x54,0x45,0x53,0x21};
+uint8_t secretString[16] = {0x21, 0x53, 0x45, 0x54, 0x52, 0x20, 0x6F, 0x74, 0x20, 0x65, 0x6D, 0x6F, 0x63, 0x6C, 0x65, 0x57};
+uint8_t decodedString[16];
+
 double result = 1234;
 
 
 int main() {
 
-	// part 1 of the assignment
+	// part 1  - blinky
 	part1();
 
-	// part 1 of the assignment
+	// part 1b - blinky 2
 	part1b();
 
-	// part 2 of the assignment
+	// part 1c - printing over serial
+	part1c();
+	
+	// part 2  - debugging
 	part2();
 
 	// extension component
@@ -44,6 +65,7 @@ int main() {
 	}
 
 }
+
 
 /*
 *	Part 1 - objective: Upload basic code to the board,
@@ -87,24 +109,62 @@ void part1b() {
 	}
 }
 
-/*
-*	Part 2 - Use the debugger for this next part of the assignment.
-*	In the variables window, write down the contents of 'secretString'
-*	as processed by the function below. Set an appropriate breakpoint
-*	and utilize 'Step Into' to monitor the changing local variables.
-*/
-void part2() {
-  uint8_t i;
-  string decode = "";
 
-  for(i = 0; i < 16; ++i){
-    decode = secretString[i];
-  }
+/* Here we want to go through some basic printing to terminal.
+*  See the top of the file if you have issues printing over USBSerial
+*  Write a line to print in hex the literal below
+*  Try a few more here, what else can you print?
+*/
+void part1c() {
+	uint8_t anInt = 33;
+	float aFloat = 33.033;
+
+	// hello world
+	serial.printf("Hello World!\n\r");
+	
+	// print an integer - with a few format options
+	serial.printf("An integer: %d\n\r", anInt);
+	serial.printf("An integer: %03d\n\r", anInt);
+	serial.printf("An integer: %07d\n\r", anInt);
+
+	// print a float - different levels of precision
+	serial.printf("An integer: %f\n\r", aFloat);
+	serial.printf("An integer: %.2f\n\r", aFloat);
+	serial.printf("An integer: %05.5f\n\r", aFloat);
+
+	// see more formatting here:
+	// https://alvinalexander.com/programming/printf-format-cheat-sheet/
+
+
+	// your task - print me!
+	uint16_t hexLiteral = 0x1234;
 
 }
 
+
 /*
-*	Extension - Using the 1st letter decoded in part 2. Find the number
+*	Part 2 - Use the debugger for this next part of the assignment.
+*	From the variables window, write down the contents of 'secretString'
+*	as it is decoded by the function below. Set an appropriate breakpoint
+*	and utilize 'Step Into' to monitor the changing local variables.
+* 	Write a comment to show that you decoded the string!
+*/
+void part2() {
+	uint8_t i;
+
+	//decode the string
+	for(i = 0; i < 16; i++){
+		decodedString[i] = secretString[15-i];
+	}
+
+	// erase the decoded string
+	for(i = 0; i < 16; i++){
+		decodedString[i] = 0x00;
+	}
+}
+
+/*
+*	Extension - Find the number
 *	that corresponds to the letter (a=1, b=2, ... , y=25, z=26) and input
 *	the number in the below function for the 'key' variable. Set an 
 *	appropriate breakpoint and report the value of the 'result' variable.
